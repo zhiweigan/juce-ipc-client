681b34d8b1fbbb65ff9f3d737b0f01b0eeca1a94
Zhi Wei Gan <zhiwei_gan@hotmail.com>
2020-02-21 14:36:46 -0500

    Rename, fixed example

--- example.js
+++ example.js
@@ -1,10 +1,12 @@
-const IpcClient = require("./client.js")
+const IpcClient = require("osc-ipc-client")
 
+// header has a default value of 0xf2b49e2c, 
+// which is the default header which JUCE uses for its IPC implementation.
 const options = {
   targetPort: 9999,
   targetHost: '127.0.0.1',
-  // header: 0xf2b49e2c, //default value
-  timeout: 3000,
+  header: 0xf2b49e2c, 
+  timeout: 3000, 
 }
 
 const client = new IpcClient(options);
@@ -16,16 +18,19 @@ oscObject = {
   ],
 }
 
+// event 'res' occurs when a response is recieved from the server.
 client.on("res", (data, address)=>{
   console.log(data, 'from', address);
 });
 
+// event 'connect' occurs when the TCP connection is connected.
 client.on("connect", ()=>{
-  client.sendOsc([oscObject, oscObject]).then(
-    client.sendOsc(oscObject).then(()=>{
-    //   client.close();
-    })
-  );
+  client.sendOsc([oscObject, oscObject])
+  .then(client.sendOsc(oscObject)
+  .then(client.sendString("hello")
+  .then(()=>{
+      client.close();
+  })));
 });
 
 
